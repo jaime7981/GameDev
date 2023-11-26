5d506b64f54f5d80502ae9236ec107f35bc8a3bb
jaime7981 <jmfernandez1@miuandes.cl>
2023-11-26 20:42:53 -0300

    folders for assets and classes

---
+++ assets/load_assets.py
@@ -0,0 +1,4 @@
+
+class LoadAssets():
+    def __init__(self) -> None:
+        pass
---
+++ classes/player.py
@@ -0,0 +1,6 @@
+import pygame
+from assets.load_assets import LoadAssets
+
+class Player:
+    def __init__(self) -> None:
+        pass

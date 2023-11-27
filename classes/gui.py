84ad7553d9226c4219fd8fbeb26814b3b3cbabaa
jaime7981 <jmfernandez1@miuandes.cl>
2023-11-27 19:35:09 -0300

    Refactor GUI class and add player movement functionality

--- classes/gui.py
+++ classes/gui.py
@@ -1,8 +1,14 @@
 import pygame
-import math
+from assets.load_assets import get_asset_resource_path
+from classes.player import Player
+
+RED = (255,0,0)
+BLUE = (0,0,255)
+YELLOW = (0,255,0)
+BACKGROUND_COLOR = (100,60,180)
 
 class GUI():
-    def __init__(self, width = 500, height = 500, assets_path = './assets') -> None:
+    def __init__(self, width = 500, height = 500) -> None:
         self.screen = pygame.display.set_mode((width, height))
         self.clock = pygame.time.Clock()
         self.running = True
@@ -14,12 +20,16 @@ class GUI():
         pygame.font.init()
         self.font = pygame.font.SysFont('Arial', 10)
 
+        pygame.display.set_caption("Juego de Prueba")
 
-        loaded_backgound = pygame.image.load(f'{assets_path}/background.jpg')
+        self.background_colorcolor = BACKGROUND_COLOR
+        loaded_backgound = pygame.image.load(get_asset_resource_path('background.jpg'))
         self.background = pygame.transform.scale(loaded_backgound, (width, height))
 
-        loaded_arrow = pygame.image.load(f'{assets_path}/arrow.png')
+        self.initialize_players()
 
+    def initialize_players(self):
+        self.player = Player()
 
     def draw_background(self) -> None:
         self.screen.blit(self.background, (0, 0))
@@ -30,13 +40,43 @@ class GUI():
         pygame.draw.circle(self.screen, (0, 10, 155), (x, y), 5)
 
 
+    def draw_player(self) -> None:
+        self.player.draw(self.screen)
+
+    def draw_plants(self) -> None:
+        for plant in self.plants:
+            plant.draw(self.screen)
+
+
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-            
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 print('mouse down')
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.player.direction = 0
+                    self.player.move_left = True
+                if event.key == pygame.K_RIGHT:
+                    self.player.direction = 1
+                    self.player.move_right = True
+                if event.key == pygame.K_UP:
+                    self.player.move_up = True
+                if event.key == pygame.K_DOWN:
+                    self.player.move_down = True
+                
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    self.player.move_left = False
+                if event.key == pygame.K_RIGHT:
+                    self.player.move_right = False
+                if event.key == pygame.K_UP:
+                    self.player.move_up = False
+                if event.key == pygame.K_DOWN:
+                    self.player.move_down = False
 
 
     def run_game(self) -> None:
@@ -46,6 +86,11 @@ class GUI():
 
             self.draw_background()
             self.draw_mouse_position()
+            self.draw_plants()
+
+            self.player.move()
+            self.screen.fill(self.color)
+            self.player.run()
 
             pygame.display.update()
 
--- main.py
+++ main.py
@@ -1,68 +1,16 @@
-import pygame
+import pygame, sys
 from classes.player import Player
-import sys
+from classes.gui import GUI
+from classes.plant.plant import Plant
 
 #COSTINANTS
 WIDTH = 800
 HIGH = 600
- 
-RED = (255,0,0)
-BLUE = (0,0,255)
-YELLOW = (0,255,0)
-BACKGROUND_COLOR = (100,60,180)
-
-clock = pygame.time.Clock()
-
-
-class Game:
-    def __init__(self) -> None:
-        pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH, HIGH))
-        pygame.display.set_caption("Juego de Prueba")
-        self.game_over = False
-        self.color = BACKGROUND_COLOR
-        self.player = Player(self)
-        self.speed_player = 2
-        #self.background = pygame.image.load("Fondo.png").convert()
-
-        
-
-    def run_game(self):
-            while not self.game_over:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        sys.exit()
-
-                    elif event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_LEFT:
-                            self.player.direction = 0
-                            self.player.move_left = True
-                        if event.key == pygame.K_RIGHT:
-
-                            self.player.direction = 1
-                            self.player.move_right = True
-                        if event.key == pygame.K_UP:
-                            self.player.move_up = True
-                        if event.key == pygame.K_DOWN:
-                            self.player.move_down = True
-                        
-                    elif event.type == pygame.KEYUP:
-                        if event.key == pygame.K_LEFT:
-                            self.player.move_left = False
-                        if event.key == pygame.K_RIGHT:
-                            self.player.move_right = False
-                        if event.key == pygame.K_UP:
-                            self.player.move_up = False
-                        if event.key == pygame.K_DOWN:
-                            self.player.move_down = False
-                
-                #self.screen.blit(self.background, [0,0])
-                self.player.move()
-                self.screen.fill(self.color)
-                self.player.run()
-                pygame.display.flip()
-                clock.tick(90) #FPS
 
 if __name__ == "__main__":
-    game = Game()
-    game.run_game()
+    gui = GUI(
+        width=WIDTH, 
+        height=HIGH
+    )
+
+    gui.run_game()

fbe004107a3bec6b5bed535747d9df3b942c63c7
jaime7981 <jmfernandez1@miuandes.cl>
2023-11-27 21:10:56 -0300

    Refactor get_asset_resource_path to include split_folders function

--- assets/load_assets.py
+++ assets/load_assets.py
@@ -3,8 +3,17 @@ import os
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ASSETS_PATH = os.path.join(BASE_PATH, 'assets')
 
+def split_folders(path):
+    final_path = ASSETS_PATH
+    splited_path = path.split('/')
+
+    for i in splited_path:
+        final_path = os.path.join(final_path, i)
+
+    return final_path
+
 def get_asset_resource_path(asset_name):
-    return os.path.join(ASSETS_PATH, asset_name)
+    return os.path.join(ASSETS_PATH, split_folders(asset_name))
 
 class LoadAssets():
     def __init__(self) -> None:
